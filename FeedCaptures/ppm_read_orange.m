bc5b1add5c36358f6e14da0283474d793f5ada40
q <qq@dhcp-140-254-202-122.osuwireless.ohio-state.edu>
2011-11-12 22:40:12 -0500

    orange ball thresholds found

---
+++ FeedCaptures/ppm_read_orange.m
@@ -0,0 +1,51 @@
+N = 13;
+Imgs = zeros(240,320,3,N);
+for i=1:N
+    file = sprintf('Orange in Lab/it/pixbuff_%d.ppm', i);
+    Imgs(:,:,:,i) = imread(file);
+end
+
+% verify myrgb2hsv works as expected
+% [h s v] = myrgb2hsv(Img(1,1,:))
+sigma = 2;
+G = fspecial('gaussian', 2*ceil(3*sigma)+1, sigma);
+
+
+% test threshold
+Masks = zeros(240,320,N);
+for i = 1:N
+    img = uint8(Imgs(:,:,:,i));
+    img = imfilter(img, G, 'replicate');
+    HSVImg = rgb2hsv(img);
+
+    Mask = zeros(240,320);
+    for row = 1:size(Masks,1)
+        for col = 1:size(Masks,2)
+           hue = HSVImg(row,col,1);
+           sat = HSVImg(row,col,2);
+           val = HSVImg(row,col,3);
+           % highlights
+           if val >= .99 && (hue < 30/360 || hue > 355/360) ...
+               && sat > .2
+               Mask(row,col) =1;
+           end
+           if (hue > 5/360 && hue < 15/360) && sat > .55
+               Mask(row,col) =1;
+           end
+           if (hue < 25/360 || hue > 358/360) && sat > .6
+               Mask(row,col) =1;
+           end
+           % shadows
+           if (val < .6 && val > .3) ...
+                   && (hue < 20/360 || hue > 355/360) && sat > .65
+               Mask(row,col) =1;
+           end
+        end
+    end
+    subplot(1,2,1);
+    imshow(Mask);
+    subplot(1,2,2);
+    imshow(uint8(img));
+    pause;
+end
+
