d78698046db9c7d6cf624e97a625313f2cac2c9f
q <qq@dhcp-140-254-203-63.osuwireless.ohio-state.edu>
2011-11-15 17:29:21 -0500

    working mask test with gaussian blurring

--- ctest/mask_test.c
+++ ctest/mask_test.c
@@ -1,4 +1,5 @@
 #include "hsv.h"
+#include "gauss.h"
 #include "ppm.h"
 #include "threshold.h"
 #include <stdio.h>
@@ -20,7 +21,23 @@ int main(int argc, char *argv[]) {
   // get mask
   orange_thresh(hsv_buf, mask_buf);
   // write mask
-  write_ppm1(mask_buf, "mask.ppm");
-  printf("Confirm that the binary image mask.ppm is a blurred copy of rgb image %s.\n", argv[1]);
+  write_ppm1(mask_buf, "raw_mask.ppm");
+  printf("Confirm that the binary image 'raw_mask.ppm' is a masked copy of rgb image '%s'.\n", argv[1]);
+
+  // ==================
+  // Test Gaussian Blur
+  // ==================
+  // load ppm
+  read_ppm(rgb_buf, argv[1]);
+  gauss_blur(rgb_buf);
+  // convert to hsv
+  rgb2hsv(rgb_buf, hsv_buf);
+  // get mask
+  orange_thresh(hsv_buf, mask_buf);
+  // write mask
+  write_ppm1(mask_buf, "blur_mask.ppm");
+  printf("Confirm that the binary image 'blur_mask.ppm' is a masked copy of blurred rgb image '%s'.\n", argv[1]);
+
+  
   return 0;
 }
