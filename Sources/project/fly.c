e0a5f739eecc644a88080edaf5f67dd5cbb6e62a
Andrew <andrewyates.name@gmail.com>
2011-11-20 21:38:10 -0500

    disabled hover mode

--- Sources/project/fly.c
+++ Sources/project/fly.c
@@ -41,8 +41,8 @@ int fly(int x, int y, int mass, int debug) {
   if (yaw > 1.0) yaw = 1.0;
   if (yaw < -1.0) yaw = -1.0;
   
-  // first bit: 0=hover mode; 1=dynamic turns
-  ardrone_at_set_progress_cmd(0, phi, theta, -gaz, yaw);
+  // first bit: 0=hover mode; second bit 1=dynamic turns
+  ardrone_at_set_progress_cmd(1, phi, theta, -gaz, yaw);
 
   
   if (debug) {
